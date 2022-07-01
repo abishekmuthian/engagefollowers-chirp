25e7b8dfc878761a8489eda131a7d7b5a4db9d22
abishek <heavyinfo@gmail.com>
2022-07-01 08:52:36 +0530

    Added mailchimp user list, featured image and meta fields.

--- src/app/actions/home.go
+++ src/app/actions/home.go
@@ -30,6 +30,12 @@ func HandleHome(w http.ResponseWriter, r *http.Request) error {
 	view.AddKey("home", 1)
 
 	view.AddKey("meta_title", config.Get("meta_title"))
+	view.AddKey("meta_url", config.Get("meta_url"))
+	view.AddKey("meta_image", config.Get("meta_image"))
+	view.AddKey("meta_title", config.Get("meta_title"))
+	view.AddKey("meta_desc", config.Get("meta_desc"))
+	view.AddKey("meta_keywords", config.Get("meta_keywords"))
+	view.AddKey("meta_twitter", config.Get("meta_twitter"))
 
 	view.Template("app/views/home.html.got")
 
--- src/app/legal.go
+++ src/app/legal.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/abishekmuthian/engagefollowers/src/lib/server/config"
 	"github.com/abishekmuthian/engagefollowers/src/lib/stats"
 	"github.com/abishekmuthian/engagefollowers/src/lib/view"
 	"net/http"
@@ -13,6 +14,13 @@ func HandleLegal(w http.ResponseWriter, r *http.Request) error {
 
 	// Render the template
 	view := view.NewRenderer(w, r)
+	view.AddKey("meta_title", config.Get("meta_title"))
+	view.AddKey("meta_url", config.Get("meta_url"))
+	view.AddKey("meta_image", config.Get("meta_image"))
+	view.AddKey("meta_title", config.Get("meta_title"))
+	view.AddKey("meta_desc", config.Get("meta_desc"))
+	view.AddKey("meta_keywords", config.Get("meta_keywords"))
+	view.AddKey("meta_twitter", config.Get("meta_twitter"))
 
 	view.Template("app/views/legal.html.got")
 
